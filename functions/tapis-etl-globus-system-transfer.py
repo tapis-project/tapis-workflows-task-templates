0ebe52a42f73c266bd3accc06bf2153697697996
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-11-17 11:43:50 -0600

    fix destinationURI in globus system transfer function

--- functions/tapis-etl-globus-system-transfer.py
+++ functions/tapis-etl-globus-system-transfer.py
@@ -37,17 +37,16 @@ try:
     # Create transfer task
     elements = []
     for f in manifest.files:
-        # FIXME perhaps it would be better to pass the system id in the manifest
         from_system_id = re.search(r"^tapis:\/{2}([^/]+)\/[\s\S]*$", f.get("url")).group(1)
-        file_path = f.get("url").replace(from_system_id, remote_inbox_system_id).replace(f.get("name"), "")
         elements.append({
-            # TODO FIXME Everything from .replace onward should be delete as soon as the insert
-            # operation is available on Globus-type endpoints
+            # FIXME perhaps it would be better to pass the system id in the manifest
+            # TODO FIXME .replace of system name in destinationURI should be deleted as soon as the insert
+            # operation is available for Globus-type systems
             "sourceURI": f.get("url").replace(from_system_id, remote_inbox_system_id),
             "destinationURI": os.path.join(
-                file_path,
+                f.get("url").rstrip("/", 2)[0].replace(from_system_id, remote_inbox_system_id),
                 destination_path.lstrip("/"),
-                f.get("name").lstrip("/")
+                f.get("name")
             )
         })
     pprint(elements)

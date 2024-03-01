2c0e91d9e7035cd35cfce1f766558b8e3ffd94ed
dhannywi <dwindrakusuma@gmail.com>
2024-03-01 23:19:42 +0000

    removed typo

--- functions/data-preprocess.py
+++ functions/data-preprocess.py
@@ -6,13 +6,12 @@ try:
     text = ctx.get_input("TEXT")
     
     text_strp = text.replace('\n', ' ')
-    text_strp = text.replace('\n', ' ')
     if len(text_strp) > 990:
         text_strp = text_strp[:990]
     prompt = "Keywords: " + text_strp
 
     ctx.set_output("PROMPT", prompt)
     ctx.stdout("Text has been pre-processed, Prompt is ready for inference")
-    
+
 except Exception as e:
     ctx.stderr(1, f"Failed to pre-process text: {e}")

fbc722a3b16fb971a31a09c8e844285e8ded7598
dhannywi <dwindrakusuma@gmail.com>
2024-03-01 23:06:48 +0000

    created function to pre-process web-scraped text into correct prompt format

---
+++ functions/data-preprocess.py
@@ -0,0 +1,18 @@
+#-------- Workflow Context import: DO NOT REMOVE ----------------
+from owe_python_sdk.runtime import execution_context as ctx
+#-------- Workflow Context import: DO NOT REMOVE ----------------
+
+try:
+    text = ctx.get_input("TEXT")
+    
+    text_strp = text.replace('\n', ' ')
+    text_strp = text.replace('\n', ' ')
+    if len(text_strp) > 990:
+        text_strp = text_strp[:990]
+    prompt = "Keywords: " + text_strp
+
+    ctx.set_output("PROMPT", prompt)
+    ctx.stdout("Text has been pre-processed, Prompt is ready for inference")
+    
+except Exception as e:
+    ctx.stderr(1, f"Failed to pre-process text: {e}")
